c18c9344fd74c61b409f94a5e246974fd1dc8bdf
rebecacrina <cuciula_rebeca@yahoo.com>
2014-04-06 22:32:55 +0300

    the changes

--- src/com/animal/farm/animals/Animal.java
+++ src/com/animal/farm/animals/Animal.java
@@ -3,6 +3,20 @@ package com.animal.farm.animals;
 public abstract class Animal {
 	protected String name;
 	protected int numberOfLegs;
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNumberOfLegs(int numberOfLegs) {
+		this.numberOfLegs = numberOfLegs;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public int getNumberOfLegs() {
+		return numberOfLegs;
+	}
 	public abstract void rawr();
 }
--- src/com/animal/farm/animals/Insect.java
+++ src/com/animal/farm/animals/Insect.java
@@ -1,7 +1,7 @@
 package com.animal.farm.animals;
 
-public class Insect extends Animal{
-	public void rawr(){
+public class Insect extends Animal {
+	public void rawr() {
 		System.out.print("I am an insect!");
 	}
 
